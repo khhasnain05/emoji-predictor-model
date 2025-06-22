551d989af026ed93436f0ac3fdf97f0126943072
Khawaja Hasnain <khawajahasnain948@gmail.com>
2025-06-22 18:16:02 +0500

    app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from transformers import AutoModelForSequenceClassification, AutoTokenizer, pipeline
+import gradio as gr
 
 # Load model and tokenizer from saved folder
-model = AutoModelForSequenceClassification.from_pretrained(r"D:/complete-emoji-model")
+model = AutoModelForSequenceClassification.from_pretrained(r"D:/complete-emoji-model") # Change path according to your own
 tokenizer = AutoTokenizer.from_pretrained(r"D:/complete-emoji-model")
 
-# Emoji ID to Emoji character
+emoji_classifier = pipeline("text-classification", model=model, tokenizer=tokenizer)
+
+# Converting Emoji ID to Emoji character
 id2emoji = {
     0: '❤',   1: '😍',  2: '😂',  3: '💕',  4: '🔥',
     5: '😊',  6: '😎',  7: '✨',  8: '💙',  9: '😘',
@@ -12,16 +15,12 @@ id2emoji = {
     15: '💯', 16: '😁', 17: '🎄', 18: '📸', 19: '😜'
 }
 
-emoji_classifier = pipeline("text-classification", model=model, tokenizer=tokenizer)
-
 def predict_emoji(text):
     result = emoji_classifier(text)
     label = int(result[0]["label"].replace("LABEL_", ""))
     return f"{id2emoji[label]}"
 
 # Create User Interface
-import gradio as gr
-
 iface = gr.Interface(
     fn=predict_emoji,
     inputs=gr.Textbox(lines=2, placeholder="Type a message here..."),
